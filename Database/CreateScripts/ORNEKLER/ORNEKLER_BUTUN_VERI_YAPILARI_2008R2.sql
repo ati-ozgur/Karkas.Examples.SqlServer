d3a4e3cd8dc7e1c9b9f7d76dcd9af6f771723837
Atilla Ozgur <ati.ozgur@gmail.com>
2013-07-28 23:35:34 +0300

    new Create Table Script added

---
+++ Database/CreateScripts/ORNEKLER/ORNEKLER_BUTUN_VERI_YAPILARI_2008R2.sql
@@ -0,0 +1,58 @@
+USE [KARKAS_ORNEK]
+GO
+
+/****** Object:  Table [ORNEKLER].[BUTUN_VERI_YAPILARI_2008R2]    Script Date: 07/28/2013 23:30:48 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [ORNEKLER].[BUTUN_VERI_YAPILARI_2008R2](
+	[BigIntColumn] [bigint] NULL,
+	[BinaryColumn] [binary](50) NULL,
+	[BitColumn] [bit] NULL,
+	[CharColumn] [char](10) NULL,
+	[DateColumn] [date] NULL,
+	[DateTimeColumn] [datetime] NULL,
+	[DateTime2Column] [datetime2](7) NULL,
+	[DateTimeOffsetColumn] [datetimeoffset](7) NULL,
+	[DecimalColumn] [decimal](18, 0) NULL,
+	[FloatColumn] [float] NULL,
+	[GeopgraphyColummn] [geography] NULL,
+	[GeometryColumn] [geometry] NULL,
+	[HierarchyIdColumn] [hierarchyid] NULL,
+	[ImageColumn] [image] NULL,
+	[IntColumn] [int] NULL,
+	[MoneyColumn] [money] NULL,
+	[NCharColumn] [nchar](10) NULL,
+	[NTextColumn] [ntext] NULL,
+	[NumericColumn] [numeric](18, 0) NULL,
+	[NVarcharColumn] [nvarchar](50) NULL,
+	[NVarcharMaxColumn] [nvarchar](max) NULL,
+	[RealColumn] [real] NULL,
+	[smallDateTimeColumn] [smalldatetime] NULL,
+	[smallIntColumn] [smallint] NULL,
+	[SmallMoneyColumn] [smallmoney] NULL,
+	[SqlVariantColumn] [sql_variant] NULL,
+	[TextColumn] [text] NULL,
+	[TimeColumn] [time](7) NULL,
+	[TimestampColumn] [timestamp] NULL,
+	[TinyIntColumn] [tinyint] NULL,
+	[UniqueIdentifierColumn] [uniqueidentifier] NULL,
+	[VarBinaryColumn] [varbinary](50) NULL,
+	[VarBinaryMaxColumn] [varbinary](max) NULL,
+	[VarCharColumn] [varchar](50) NULL,
+	[VarCharMaxColumn] [varchar](max) NULL,
+	[XmlColumn] [xml] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
